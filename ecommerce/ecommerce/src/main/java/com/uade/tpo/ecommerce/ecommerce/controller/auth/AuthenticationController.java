377caee4f4571a091e98564ae53038f5f36739d7
ivan <43122250+iivigarcia@users.noreply.github.com>
2024-09-24 16:14:58 -0300

    pruebas de seguridad

--- ecommerce/ecommerce/src/main/java/com/uade/tpo/ecommerce/ecommerce/controller/auth/AuthenticationController.java
+++ ecommerce/ecommerce/src/main/java/com/uade/tpo/ecommerce/ecommerce/controller/auth/AuthenticationController.java
@@ -1,11 +1,11 @@
 package com.uade.tpo.ecommerce.ecommerce.controller.auth;
+import com.uade.tpo.ecommerce.ecommerce.dto.UpdatePasswordDTO;
 import com.uade.tpo.ecommerce.ecommerce.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/auth")
